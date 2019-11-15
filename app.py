c6002655b977d72dea986fbc30651f179186ad1f
Cheryl Murphy <webmaster@cherylli.com>
2019-11-15 22:59:43 +1100

    Add currency conversion
    Add currency conversion with temporary variables

--- app.py
+++ app.py
@@ -10,16 +10,32 @@ app.config['DEBUG'] = True
 @app.route('/')
 def index():
     request = requests.get('https://api.exchangeratesapi.io/latest').json()
+
+    # show json string in log
     app.logger.info(request)
 
+    # test block
     rates = {
         'base': request['base'],
         'date': request['date'],
-        'AUD': request['rates']['AUD']
+        'AUD': request['rates']['AUD'],
+        'USD': request['rates']['USD']
     }
 
     app.logger.info(rates)
+    # end test block
+
+    # test conversion function
+    fromCurrency = 'USD'
+    toCurrency = 'AUD'
+    app.logger.info(
+        f"{fromCurrency} to {toCurrency} = {conversion(1,rates[fromCurrency],rates[toCurrency])}")
+
     return render_template('index.html')
+
+
+def conversion(amount, fromCurrency, toCurrency):
+    return round(amount * fromCurrency/toCurrency, 3)
 
 
 if __name__ == "__main__":
