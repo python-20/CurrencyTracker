8239f0fa6b10cb0dc08bd64f3593b22fcd85df4f
Cheryl Murphy <webmaster@cherylli.com>
2019-11-15 13:55:34 +1100

    Update app.py
    add api call and basic parsing

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+import requests
+
 # import config
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 
 @app.route('/')
 def index():
+    request = requests.get('https://api.exchangeratesapi.io/latest').json()
+    app.logger.info(request)
+
+    rates = {
+        'base': request['base'],
+        'date': request['date'],
+        'AUD': request['rates']['AUD']
+    }
+
+    app.logger.info(rates)
     return render_template('index.html')
 
 
