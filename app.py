ea1500c9b6318670e0ae1aa599cf61ea964dd959
Cheryl Murphy <webmaster@cherylli.com>
2019-11-17 21:13:20 +1100

    Update index.html
    Update index.html with basic controls to test backend
    Update base.html with title block

--- app.py
+++ app.py
@@ -29,3 +29,9 @@ def conversion(amount, fromCurrency, toCurrency):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+@app.route('/', methods=['POST'])
+def index_post():
+    amount = request.form['amount']
+    return amount
